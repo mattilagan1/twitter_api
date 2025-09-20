3b9e8683b6680ddc441423d1f309527ba026e845
mattilagan1 <ilagan.matthew67@gmail.com>
2025-09-20 14:09:10 +1000

    added user controller and a tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_20_035539) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_20_040816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "tweet_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
